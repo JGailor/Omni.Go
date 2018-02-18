8565fb1f8ef6ff57deadf158685a4aa85d72cf52
Jeremy Gailor <jgailor@gmail.com>
2018-02-18 13:48:56 -0800

    Major refactoring to reusable methods for making JSON RPC requests; removing Gorilla dependency because of it's apparent inability to pass 0 args... might be a misunderstanding on my part of either the library or Go, but it's easy enough to implement here

--- rpc.go
+++ rpc.go
@@ -1,149 +1,163 @@
 package omnigo
 
 import (
-    "bytes"
-    "github.com/gorilla/rpc/json"
-    "net/http"
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"math/rand"
+	"net/http"
 )
 
-func makeJsonRpcRequest(url, responseTarget interface{}, args interface{}) err {
-    msg, err := json.EncodeClient("omni", args)
-    if err != nil {
-        return err
-    }
-
-    req, err := http.NewRequest("POST", url, bytes.NewBuffer(msg))
-    if err != nil {
-        return err
-    }
-
-    req.Header.Set("Content-Type", "application/json")
-    client := new(http.Client)
-    resp, err := client.Do(req)
-    if err != nil {
-        return err
-    }
-
-    defer resp.Body.Close()
-    err = json.DecodeClientResponse(resp.Body, responseTarget)
-    if err != nil {
-        return err
-    }
+type OmniRpcClient struct {
+	Url      string
+	Username string
+	Password string
 }
 
-func OmniSend(fromAddress, toAddress string, propertyId int, amount, redeemAddress, referenceAmount string) string {
-    msg, err := json.EncodeClient("omni", [fromAddress, toAddress, propertyId, amount, redeemAddress, referenceAmount])
-    if err != nil {
-        return err
-    }
-
-    req, err := http.NewRequest("POST", url, bytes.NewBuffer(msg))
-    if err != nil {
-        return err
-    }
-
-    req.Header.Set("Content-Type", "application/json")
-    client := new(http.Client)
-    resp, err := client.Do(req)
-    if err != nil {
-        return err
-    }
-
-    defer resp.Body.Close()
-    var transactionHash string
-    err = json.DecodeClientResponse(resp.Body, &transactionHash)
-    if err != nil {
-        return err
-    }
-
-    return nil
+type clientRequest struct {
+	Method string        `json:"method"`
+	Params []interface{} `json:"params"`
+	Id     uint64        `json:"id"`
 }
 
-func OmniSendDexSell(fromAddress string, propertyIdForSale int, amountForSale, amountDesired string, paymentWindow int, minAcceptFee string, action int) string {
-    msg, err := json.EncodeClientRequest("omni_senddexsell", [fromAddress, propertyIdForSale, amountForSale, amountDesired, paymentWindow, minAcceptFee, action])
-    if err != nil {
-        return "", err
-    }
-
-    req, err := http.NewRequest("POST", url, bytes.NewBuffer(msg))
-    if err != nil {
-        return "", err
-    }
-
-    req.Header.Set("Content-Type", "application/json")
-    client := new(http.Client)
-    resp, err := client.Do(req)
-    if err != nil {
-        return "", err
-    }
-
-    defer resp.Body.Close()
-
-    var transactionHash string
-    err = json.DecodeClientResponse(resp.Body, &transactionHash)
-    if err != nil {
-        return "", err
-    }
-
-    return transactionHash, nil
+type clientResponse struct {
+	Result *json.RawMessage `json:"result"`
+	Error  interface{}      `json:"error"`
+	Id     uint64           `json:"id"`
 }
 
-func OmniSendDexAccept(fromAddress, toAddress string, propertyId int, amount string, override bool) string {
-    var transactionHash string
-    err := makeJsonRpcRequest(url, &transactionHash, fromAddress, toAddress, propertyId, amount, override)
-    if err != nil {
-        return "", err
-    }
+func encodeClientRequest(method string, args ...interface{}) ([]byte, error) {
+	var params []interface{}
 
-    return transactionHash, nil
+	if len(args) == 0 {
+		params = make([]interface{}, 0)
+	} else {
+		params = args
+	}
+
+	c := &clientRequest{
+		Method: method,
+		Params: params,
+		Id:     uint64(rand.Int63()),
+	}
+
+	return json.Marshal(c)
 }
 
-func OmniSendIssuanceCrowdSale(fromAddress string, ecosystem, type, previousId number, category, subCategory, name, url, data string, propertyIdDesired int, tokensPerUnit string, deadline, earlyBonus, issuerPercentage int) string {
-    var transactionHash string
-    err := makeJsonRpcRequest(url, &transactionHash, fromAddress, ecosystem, type, previousId, category, subCategory, name, url, data, propertyIdDesired, tokensPerUnit, deadline, earlyBonus, issuerPercentage)
-    if err != nil {
-        return "", err
-    }
+func decodeClientResponse(r io.Reader, reply interface{}) error {
+	var c clientResponse
+	if err := json.NewDecoder(r).Decode(&c); err != nil {
+		return err
+	}
 
-    return transactionHash, nil
+	if c.Error != nil {
+		return fmt.Errorf("%v", c.Error)
+	}
+
+	if c.Result == nil {
+		return errors.New("result is null")
+	}
+
+	return json.Unmarshal(*c.Result, reply)
 }
 
-func OmniSendIssuanceFixed(fromAddress string, ecosystem, type, previousId int, category, subCategory, name, url, data, amount, tokensPerUnit string, deadline, earlyBonus, issuerPercentage int) string {
-    var transactionHash string
-    err := makeJsonRpcRequest(url, &transactionHash, fromAddress, ecosystem, type, previousId, category, subCategory, name, url, data, amount, tokensPerUnit, deadline, earlyBonus, issuerPercentage)
-    if err != nil {
-        return "", err
-    }
+func (omni *OmniRpcClient) invokeJsonRpcMethod(decodeTarget interface{}, method string, args ...interface{}) error {
+	msg, err := encodeClientRequest("omni_getinfo")
 
-    return transactionHash, nil
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("POST", omni.Url, bytes.NewBuffer(msg))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.SetBasicAuth(omni.Username, omni.Password)
+
+	client := new(http.Client)
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	err = decodeClientResponse(resp.Body, &decodeTarget)
+	if err != nil && err != io.EOF {
+		return err
+	}
+
+	return nil
 }
 
+func (omni *OmniRpcClient) OmniSendDexSell(fromAddress string, propertyIdForSale int, amountForSale, amountDesired string, paymentWindow int, minAcceptFee string, action int) (string, error) {
+	var txHash string
+	err := omni.invokeJsonRpcMethod(&txHash, "omni_senddexsell", fromAddress, propertyIdForSale, amountForSale, amountDesired, paymentWindow, minAcceptFee, action)
+	return txHash, err
+}
 
-func OmniGetAllBalancesForAddress(address string) *[]Balance, error {
-    msg, err := json.EncodeClientRequest("omni_getallbalancesforaddress", address)
-    if err != nil {
-        return nil, err
-    }
+func (omni *OmniRpcClient) OmniSendDexAccept(fromAddress, toAddress string, propertyId int, amount string, override bool) (string, error) {
+	var txHash string
+	err := omni.invokeJsonRpcMethod(&txHash, "omni_senddexaccept", fromAddress, toAddress, propertyId, amount, override)
+	return txHash, err
+}
 
-    req, err := http.NewRequest("POST", url, bytes.NewBuffer(msg))
-    if err != nil {
-        return nil, err
-    }
+func (omni *OmniRpcClient) OmniSendIssuanceCrowdSale(fromAddress string, ecosystem, tokenType, previousId int, category, subCategory, name, url, data string, propertyIdDesired int, tokensPerUnit string, deadline, earlyBonus, issuerPercentage int) (string, error) {
+	var txHash string
+	err := omni.invokeJsonRpcMethod(&txHash, "omni_sendissuancecrowdsale", fromAddress, ecosystem, tokenType, previousId, category, subCategory, name, url, data, propertyIdDesired, tokensPerUnit, deadline, earlyBonus, issuerPercentage)
+	return txHash, err
+}
 
-    req.Header.Set("Content-Type", "application/json")
-    client := new(http.Client)
-    resp, err := client.Do(req)
-    if err != nil {
-        return nil, err
-    }
+func (omni *OmniRpcClient) OmniSendIssuanceFixed(fromAddress string, ecosystem, tokenType, previousId int, category, subCategory, name, url, data, amount, tokensPerUnit string, deadline, earlyBonus, issuerPercentage int) (string, error) {
+	var txHash string
+	err := omni.invokeJsonRpcMethod(&txHash, "omni_sendissuancefixed", fromAddress, ecosystem, tokenType, previousId, category, subCategory, name, url, data, amount, tokensPerUnit, deadline, earlyBonus, issuerPercentage)
+	return txHash, err
+}
 
-    defer resp.Body.Close()
+func (omni *OmniRpcClient) OmniSendIssuanceManaged(fromAddress string, ecosystem, tokenType, previousId int, category, subCategory, name, url, data string) (string, error) {
+	var txHash string
+	err := omni.invokeJsonRpcMethod(&txHash, "omni_sendissuancemanaged", fromAddress, ecosystem, previousId, category, subCategory, name, url, data)
+	return txHash, err
+}
 
-    balances := []Balance{}
-    err := json.DecodeClientResponse(resp.Body, &balances)
-    if err != nil {
-        return balances, err
-    }
+func (omni *OmniRpcClient) OmniSendsTo(fromAddress string, propertyId int, amount, redeemAddress string, distributionProperty int) (string, error) {
+	var txHash string
+	err := omni.invokeJsonRpcMethod(&txHash, "omni_sendsto", fromAddress, propertyId, amount, redeemAddress, distributionProperty)
+	return txHash, err
+}
 
-    return balances, nil
+func (omni *OmniRpcClient) OmniSendGrant(fromAddress, toAddress string, propertyId int, amount, memo string) (string, error) {
+	var txHash string
+	err := omni.invokeJsonRpcMethod(&txHash, "omni_sendgrant", fromAddress, toAddress, propertyId, amount, memo)
+	return txHash, err
+}
+
+func (omni *OmniRpcClient) OmniSendRevoke(fromAddress string, propertyId int, amount, memo string) (string, error) {
+	var txHash string
+	err := omni.invokeJsonRpcMethod(&txHash, "omni_sendrevoke", fromAddress, propertyId, amount, memo)
+	return txHash, err
+}
+
+func (omni *OmniRpcClient) OmniSendCloseCrowdSale(fromAddress string, propertyId int) (string, error) {
+	var txHash string
+	err := omni.invokeJsonRpcMethod(&txHash, "omni_sendclosecrowdsale", fromAddress, propertyId)
+	return txHash, err
+}
+
+func (omni *OmniRpcClient) OmniGetAllBalancesForAddress(address string) (*[]Balance, error) {
+	balances := []Balance{}
+	err := omni.invokeJsonRpcMethod(&balances, "omni_getallbalancesforaddress", address)
+
+	return &balances, err
+}
+
+func (omni *OmniRpcClient) GetInfo() (*OmniInfo, error) {
+	info := OmniInfo{}
+	err := omni.invokeJsonRpcMethod(&info, "omni_getinfo")
+
+	return &info, err
 }
--- types.go
+++ types.go
@@ -1,7 +1,26 @@
 package omnigo
 
+type OmniInfo struct {
+	OmnicoreVersionInt int
+	OmnicoreVersion    string
+	MasterCoreVersion  string
+	BitcoinCoreVersion string
+	Block              int
+	Blocktime          int
+	BlockTransactions  int
+	TotalTransactions  int
+	Alerts             []Alert
+}
+
+type Alert struct {
+	TypeInt   int
+	TokenType string
+	Expiry    string
+	Message   string
+}
+
 type Balance struct {
-    PropertyID int
-    Balance    string
-    Reserved   string
+	PropertyID int
+	Balance    string
+	Reserved   string
 }
